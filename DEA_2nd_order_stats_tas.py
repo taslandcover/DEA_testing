5f3153ea7ec6dd64f7301ba9db47ecd92b7ffeaa
taslandcover <lindsay.mitchell@dpipwe.tas.gov.au>
2018-12-20 08:32:32 +1100

    add gomedian as an output

--- DEA_2nd_order_stats_tas.py
+++ DEA_2nd_order_stats_tas.py
@@ -57,6 +57,8 @@ dsma = DEADataHandling.load_clearlandsat(dc=dc, query=query,
                                           ls7_slc_off=True)
 
 dsma = dsma.drop('data_perc')
+# compute geomedian
+dsma_gm = GeoMedian().compute(dsm)
 
 ##############################################################################
 print("...computing TernaryMAD")
@@ -70,5 +72,5 @@ ds=xr.Dataset({'smad': (['y','x'], dsma_tmad.sdev),
 print("...writing output")
 #datacube.storage.storage.write_dataset_to_netcdf(dsma_smad, '/g/data/r78/DPIPWE_LM/output_data/ls8_smad_test.nc')
 datacube.helpers.write_geotiff(filename='/g/data/r78/DPIPWE_lm/output_data/lsX_TMAD_2010_2015.tif', dataset=ds)
-datacube.storage.storage.write_dataset_to_netcdf(dsma, '/g/data/r78/DPIPWE_lm/output_data/lsX_pcm_2010_2015.nc')
+datacube.storage.storage.write_dataset_to_netcdf(dsma_gm, '/g/data/r78/DPIPWE_lm/output_data/lsX_pcm_2010_2015.nc')
 #DEADataHandling.dataset_to_geotiff('dsma_smad_netcdf_test.nc', dsma_smad)
