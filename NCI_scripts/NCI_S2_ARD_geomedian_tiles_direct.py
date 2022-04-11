21a1f38ecfa18c00e1e7d4209467d880bf2b2ad8
taslandcover <42013367+taslandcover@users.noreply.github.com>
2022-04-11 12:04:47 +1000

    Expand temporal period for load_ard

--- NCI_scripts/NCI_S2_ARD_geomedian_tiles_direct.py
+++ NCI_scripts/NCI_S2_ARD_geomedian_tiles_direct.py
@@ -1,6 +1,6 @@
 import datacube
 import os
-import pandas as pd
+#import pandas as pd
 import geopandas as gpd 
 from odc.algo import to_f32, xr_geomedian, int_geomedian
 
@@ -10,11 +10,11 @@ import sys
 sys.path.append(os.path.abspath('/g/data/r78/DPIPWE_lm/repos/dea-notebooks/Tools'))
 #from dea_datahandling import load_ard
 from dea_tools.datahandling import load_ard
-from dea_tools.dask import create_local_dask_cluster
+#from dea_tools.dask import create_local_dask_cluster
 from datacube_stats.statistics import GeoMedian
 from datacube.utils.cog import write_cog
-from datacube.drivers.netcdf import write_dataset_to_netcdf
-import xarray as xr
+#from datacube.drivers.netcdf import write_dataset_to_netcdf
+#import xarray as xr
 
 outputdir = '/g/data/r78/DPIPWE_lm/output_data/'
 if not os.path.exists(outputdir):
@@ -32,17 +32,14 @@ if label:
     index = albers[albers['label']==label].index[0]
     x = (albers.loc[index]['X_MIN'], albers.loc[index]['X_MAX'])
     y = (albers.loc[index]['Y_MIN'], albers.loc[index]['Y_MAX'])
-    #output_filename = outputdir + '/S2gm_2016-2017_'+'_'.join(label.split(','))+'.nc'
-    output_filename = outputdir + '/S2gm_2021-2022_'+'_'.join(label.split(','))
+    output_filename = outputdir + '/S2gm_2021-2022_'+'_'.join(label.split(','))+'.tif'
     print("Working on tile {}...".format(label))
 else:
     x, y = (1385000.0, 1375000.0), (-4570000.0, -4580000.0)
     if subset:
-        #output_filename = 'S2_ARD_gm_2021_test_subset.nc'
-        output_filename = 'S2_ARD_gm_2021_test_subset'
+        output_filename = 'S2_ARD_gm_2021_test_subset.tif'
     else:
-        #output_filename = 'S2_ARD_gm_2021_test_one.nc'
-        output_filename = 'S2_ARD_gm_2021_test_one'
+        output_filename = 'S2_ARD_gm_2021_test_one.tif'
         
 
 def load_ds(x, y):
@@ -50,11 +47,11 @@ def load_ds(x, y):
     query = {'x': x,
           'y': y,
           'crs': 'EPSG:3577',
-          'time': ('2021-10-01', '2021-10-10'),
-          'measurements': ['nbart_blue', 'nbart_green', 'nbart_red', 'nbart_nir_1'], # Can add nbart_swir2 for true flase colour but change res to 20
-          'resolution': (-20, 20),
+          'time': ('2021-10', '2021-04'),
+          'measurements': ['nbart_blue', 'nbart_green', 'nbart_red', 'nbart_nir_1', 'nbart_swir_2'], # Can add nbart_swir_2 for true flase colour but change res to 20
+          'resolution': (-10, 10),
           'group_by': 'solar_day',
-          'output_crs': 'EPSG:3577'}
+          'output_crs': 'EPSG:28355'} #Albers is 'output_crs': 'EPSG:3577'
     
         
     # Load available data from both Sentinel 2 satellites
