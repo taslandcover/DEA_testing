6f1ea3126926d880a22149d5ff5f1b0d64172d15
taslandcover <42013367+taslandcover@users.noreply.github.com>
2022-04-11 10:18:14 +1000

    Reconfigure to process tile subsets

--- NCI_scripts/NCI_S2_ARD_geomedian_tiles_direct.py
+++ NCI_scripts/NCI_S2_ARD_geomedian_tiles_direct.py
@@ -31,8 +31,8 @@ if label:
     index = albers[albers['label']==label].index[0]
     x = (albers.loc[index]['X_MIN'], albers.loc[index]['X_MAX'])
     y = (albers.loc[index]['Y_MIN'], albers.loc[index]['Y_MAX'])
-    #output_filename = outputdir + '/month_gm_2016-2017_'+'_'.join(label.split(','))+'.nc'
-    output_filename = outputdir + '/month_gm_2021-2022_'+'_'.join(label.split(','))
+    #output_filename = outputdir + '/S2gm_2016-2017_'+'_'.join(label.split(','))+'.nc'
+    output_filename = outputdir + '/S2gm_2021-2022_'+'_'.join(label.split(','))
     print("Working on tile {}...".format(label))
 else:
     x, y = (1385000.0, 1375000.0), (-4570000.0, -4580000.0)
@@ -43,29 +43,45 @@ else:
         #output_filename = 'S2_ARD_gm_2021_test_one.nc'
         output_filename = 'S2_ARD_gm_2021_test_one'
         
-# Set up Dask client
-#client = create_local_dask_cluster(return_client=True)       
-client = start_local_dask(mem_safety_margin = '3Gb')
 
-query = {'x': x,
+ def load_ds(x, y):
+    #This query from NCI_Geomedian_Tiles
+    query = {'x': x,
           'y': y,
           'crs': 'EPSG:3577',
-          'time': ('2021-10-01', '2022-05-15'),
+          'time': ('2021-10-01', '2021-10-10'),
           'measurements': ['nbart_blue', 'nbart_green', 'nbart_red', 'nbart_nir_1'], # Can add nbart_swir2 for true flase colour but change res to 20
           'resolution': (-20, 20),
           'group_by': 'solar_day',
           'output_crs': 'EPSG:3577'}
-
-# Lazy load data using Dask
-print('Lazy loading tile '+label)
-lazy_ds = dc.load(product=['s2a_ard_granule', 's2b_ard_granule'],
-                  dask_chunks={"time": 1, "x": 1000, "y": 1000},
+    
+        
+    # Load available data from both Sentinel 2 satellites
+    ds = load_ard(dc=dc,
+                  products=['s2a_ard_granule', 's2b_ard_granule'],
+                  dask_chunks={'time':1, "x": 1000, "y": 1000},
                   **query)
 
-# Run the Geomedian calculation
-print('Running geomedian...')
-geomedian = int_geomedian(lazy_ds)
-geomedian = geomedian.compute()
+  
+    # Compute geomedian here is necessary - either for dataset or subset months
+    geomedian = int_geomedian(ds)
+    geomedian = geomedian.compute()
+    return geomedian
+    
+    # compute geomedian
+    #ds_gm = GeoMedian().compute(ds)
+    #return ds_gm.copy()       
+        
+#Compute and then concat subsets  
+xm, ym = (x[0]+x[1])/2, (y[0]+y[1])/2
+x1, x2 = (x[0], xm), (xm, x[1])
+y1, y2 = (y[0], ym), (ym, y[1])
+if subset:
+    out1 = load_ds(x1, y)
+    out2 = load_ds(x2, y)
+    out = xr.concat([out1, out2], dim='x')
+else:
+    out = load_ds(x, y)
 
 # Here we can export the geomedian
 # for COG we need an array not a dataset
